df3a2675f34934d2f83b77361502549e485f9408
Christal Vigil <c.vigil27@gmail.com>
2020-11-29 23:15:24 -0700

    adding comments to API.js file

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,8 @@
-
-
-  
+// bring in express router and transaction.js file   
 const router = require("express").Router();
 const Transaction = require("../models/transaction.js");
 
+// routes for post requests
 router.post("/api/transaction", ({ body }, res) => {
   Transaction.create(body)
     .then(dbTransaction => {
@@ -23,7 +22,7 @@ router.post("/api/transaction/bulk", ({ body }, res) => {
       res.status(400).json(err);
     });
 });
-
+// routes for get requests 
 router.get("/api/transaction", (req, res) => {
   Transaction.find({})
     .sort({ date: -1 })
@@ -35,41 +34,5 @@ router.get("/api/transaction", (req, res) => {
     });
 });
 
-module.exports = router;// // import router for CRUD USE and import the transaction.js file 
-// const router = require("express").Router();
-// const Transaction = require("../models/transaction.js");
+module.exports = router;
 
-// // POST method 
-// router.post("/api/transaction", ({body}, res) => {
-//   Transaction.create(body)
-//     .then(dbTransaction => {
-//       res.json(dbTransaction);
-//     })
-//     .catch(err => {
-//       res.status(404).json(err);
-//     });
-// });
-
-// // POST METHOD 
-// router.post("/api/transaction/bulk", ({body}, res) => {
-//   Transaction.insertMany(body)
-//     .then(dbTransaction => {
-//       res.json(dbTransaction);
-//     })
-//     .catch(err => {
-//       res.status(404).json(err);
-//     });
-// });
-// // GET METHOD 
-// router.get("/api/transaction", (req, res) => {
-//   Transaction.find({}).sort({date: -1})
-//     .then(dbTransaction => {
-//       res.json(dbTransaction);
-//     })
-//     .catch(err => {
-//       res.status(404).json(err);
-//     });
-// });
-
-// // exporting 
-// module.exports = router;
